3ecff82109e63a5dc40394c6a411404247f1d21a
Papa Kojo Oseku-Afful <apa.afful@ashesi.edu.gh>
2020-09-22 23:53:28 +0000

    update1

---
+++ script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const middlename = document.getElementById('middlename');
+const lastname = document.getElementById('lastname');
+const phonenumber = document.getElementById('phonenumber');
+const email = document.getElementById('email');
+const address = document.getElementById('address');
+
+
+
+
+
+form.addEventListener('submit', (e)=> {
+    let messages = []
+    if(firstname.value === ''|| firstname.value == null){
+        
+    }
+        
+    
+    if(middlename.value === ''|| middlename.value == null){
+        
+    }
+
+    if(lastname.value === ''|| lastname.value == null){
+        
+    }
+    if(phonenumber.value === ''|| phonenumber.value == null){
+        
+    }
+    if(email.value === ''|| email.value == null){
+        
+    }
+    if(address.value === ''|| address.value == null){
+
+    }
+
+
+        
+
